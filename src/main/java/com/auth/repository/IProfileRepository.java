3f521d66c869f45df76aa6a6e1d275c4ff1e7ef5
Diego Veras <verasdiegoca78@gmail.com>
2024-12-30 22:02:59 -0300

    Remove unnecessary newlines in IGenericRepository.java and IProfileRepository.java

--- src/main/java/com/auth/repository/IProfileRepository.java
+++ src/main/java/com/auth/repository/IProfileRepository.java
@@ -8,10 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface IProfileRepository extends IGenericRepository<Profile, UUID> {
-
     @Query(value = "SELECT * FROM profiles p WHERE p.user_id = :userId", nativeQuery = true)
     Optional<Profile> findByUserId(@Param("userId") UUID userId);
-
     Optional<Profile> findProfileByUser_Dni(String dni);
-
 }
