195e3262aca0f4944ee91701874551658f96da90
Diego Veras <verasdiegoca78@gmail.com>
2024-12-30 22:09:43 -0300

    Remove unnecessary newline at end of file in EmailRequestDto.java

--- src/main/java/com/auth/dto/response/auth/UserResponseDto.java
+++ src/main/java/com/auth/dto/response/auth/UserResponseDto.java
@@ -1,8 +1,11 @@
 package com.auth.dto.response.auth;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.Set;
 import java.util.UUID;
 
+@JsonPropertyOrder({"userId", "email", "name", "lastname", "dni", "isVerified", "roles"})
 public record UserResponseDto(
         UUID userId,
         String email,
