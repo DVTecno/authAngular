9f7d1d3e31f616efa491100302f67f0ef338dcb5
Diego Veras <verasdiegoca78@gmail.com>
2024-12-30 22:09:43 -0300

    Add JsonPropertyOrder annotation to UserResponseDto
    - Annotate UserResponseDto with @JsonPropertyOrder to specify the order of JSON properties.

--- src/main/java/com/auth/dto/response/auth/RoleResponseDto.java
+++ src/main/java/com/auth/dto/response/auth/RoleResponseDto.java
@@ -1,6 +1,9 @@
 package com.auth.dto.response.auth;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.UUID;
 
+@JsonPropertyOrder({"roleId", "name"})
 public record RoleResponseDto(UUID roleId,String name) {
 }
