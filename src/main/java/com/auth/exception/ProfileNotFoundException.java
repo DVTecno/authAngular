557955fcb9505846fd5068dbdcb359c0eda119f6
Diego Veras <verasdiegoca78@gmail.com>
2024-12-30 22:05:57 -0300

    Remove unnecessary newline at end of file in ProfileNotFoundException.java

--- src/main/java/com/auth/exception/ProfileNotFoundException.java
+++ src/main/java/com/auth/exception/ProfileNotFoundException.java
@@ -1,11 +1,7 @@
 package com.auth.exception;
 
-
 public class ProfileNotFoundException extends RuntimeException {
-
     public ProfileNotFoundException(String message) {
         super(message);
     }
-
 }
-
