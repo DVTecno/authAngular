5063e5f2570b20d9640748d0ac61091c8d6c333b
Diego Veras <verasdiegoca78@gmail.com>
2025-01-03 16:26:18 -0300

    Update CORS configuration to allow additional origins

--- src/main/java/com/auth/config/security/SecurityConfig.java
+++ src/main/java/com/auth/config/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://login-clase.up.railway.app", "https://authangular-production.up.railway.app", "http://localhost:8090"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
